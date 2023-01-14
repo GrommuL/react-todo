4baf68d3667acd83d1ac6c831bf86cf42f8eef0d
GrommuL <miketsvg@gmail.com>
2023-01-15 01:23:55 +0600

    add register form

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Register/Register";
+import './style.scss'
 
 function App() {
   return (
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
+import './register.scss'
+import RegisterBottomDecaration from './RegisterBottomDecaration'
 
 const Register = () => {
   return (
-    <div className='login'>
-    <form action="">
-        <h2>Регистрация</h2>
-        <input type="text" placeholder='Введите логин' />
-        <input type="email" placeholder='Введите почту' />
-        <input type="password" placeholder='Введите пароль' />
-        <input type="password" placeholder='Подтвердите пароль' />
-        <button type='submit'>Зарегистрироваться</button>
+    <div className='register'>
+    <form className='form' action="">
+        <h2 className='form__title'>Регистрация</h2>
+        <input className='form__field' type="text" placeholder='Введите логин' />
+        <input className='form__field' type="email" placeholder='Введите почту' />
+        <input className='form__field' type="password" placeholder='Введите пароль' />
+        <input className='form__field' type="password" placeholder='Подтвердите пароль' />
+        <p></p>
+        <button className='form__btn' type='submit'>Зарегистрироваться</button>
     </form>
+    <RegisterBottomDecaration/>
 </div>
   )
 }

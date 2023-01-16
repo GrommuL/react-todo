cdee51efe4f673fe9489ae0c02e4666c7ce1e6f2
GrommuL <miketsvg@gmail.com>
2023-01-16 23:39:14 +0600

    add tasks

--- src/components/ContentCategory/ContentCategory.jsx
+++ src/components/ContentCategory/ContentCategory.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { CustomContext } from '../../utils/Context'
 import EditPencil from '../../pages/Home/EditPencil'
 import '../../pages/Home/homeContent.scss'
@@ -6,18 +6,21 @@ import '../../pages/Home/homeContent.scss'
 const ContentCategory = ({status, colorStatus}) => {
 
     const {user} = useContext(CustomContext)
-
+    const [complete, setComplete] = useState(false)
   return (
     <>
         <div className="content__title">
             <h2 className='content__title-name' style={{color: colorStatus}}>{status}</h2>
             <EditPencil/>
         </div>
-        <ul>
+        <ul className='content__tasks'>
         {
             status !== 'Все задачи' ?
             user.categories.find(item => item.categoryName === status).tasks.map(task => (
-                 <li key={task.id}>{task.taskTitle}</li>
+                 <li className='content__tasks-item' key={task.id}>
+                    <div className={`content__tasks-status ${task.isComplete ? 'complete' : ''}`} onClick={()=> setComplete(!complete)}></div>
+                    <p className='content__tasks-name'>{task.taskTitle}</p>
+                </li>
             )) : ''
         }
         </ul>
